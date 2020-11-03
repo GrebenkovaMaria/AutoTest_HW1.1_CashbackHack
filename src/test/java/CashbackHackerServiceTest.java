58ef69c07f14dd8356318288c78d36820460af2c
Maria Grebenkova <grebenkova.m.p@gmail.com>
2020-11-04 01:12:30 +0300

    test with Junit4

--- src/test/java/CashbackHackerServiceTest.java
+++ src/test/java/CashbackHackerServiceTest.java
@@ -1,49 +1,50 @@
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackerServiceTest {
 
-    @org.testng.annotations.Test
-    void shouldRemain0withAmountEqualsToBoundary() {
+    @org.junit.Test
+    public void shouldRemain0withAmountEqualsToBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldRemain1withAmountEquals999() {
+    @org.junit.Test
+    public void shouldRemain1withAmountEquals999() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldRemain1000withAmountEquals0() {
+    @org.junit.Test
+    public void shouldRemain1000withAmountEquals0() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 0;
         int expected = 1000;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldRemainOnewithAmountEquals999() {
+    @org.junit.Test
+    public void shouldRemainOnewithAmountEquals999() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1;
         int expected = 999;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldRemainZeroWithAmountMoreBoundary() {
+    @org.junit.Test
+    public void shouldRemainZeroWithAmountMoreBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1200;
         int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
+
