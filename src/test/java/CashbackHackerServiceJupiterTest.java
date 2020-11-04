f46acf547fdf8c04677e1ff523721b7d9ae90623
Maria Grebenkova <grebenkova.m.p@gmail.com>
2020-11-04 17:19:34 +0300

    Tests're on Jupiter

--- src/test/java/CashbackHackerServiceJupiterTest.java
+++ src/test/java/CashbackHackerServiceJupiterTest.java
@@ -1,50 +1,48 @@
-import static org.junit.Assert.*;
 
-public class CashbackHackerServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @org.junit.Test
-    public void shouldRemain0withAmountEqualsToBoundary() {
+class CashbackHackerServiceJupiterTest {
+    @ org.junit.jupiter.api. Test
+    void shouldRemain0withAmountEqualsToBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1000;
         int expected = 0;
-        int actual = service.remain(amount);
+        int actual = service.remain(amount) ;
         assertEquals(expected, actual);
     }
-
-    @org.junit.Test
-    public void shouldRemain1withAmountEquals999() {
+    @ org.junit.jupiter.api. Test
+    void shouldRemain1withAmountEquals999() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 999;
         int expected = 1;
-        int actual = service.remain(amount);
+        int actual = service.remain(amount) ;
         assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void shouldRemain1000withAmountEquals0() {
+    @ org.junit.jupiter.api. Test
+    void shouldRemain1000withAmountEquals0() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 0;
         int expected = 1000;
-        int actual = service.remain(amount);
+        int actual = service.remain(amount) ;
         assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void shouldRemainOnewithAmountEquals999() {
+    @ org.junit.jupiter.api. Test
+    void shouldRemainZeroWithAmountEquals999() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1;
         int expected = 999;
-        int actual = service.remain(amount);
+        int actual = service.remain(amount) ;
         assertEquals(expected, actual);
     }
 
-    @org.junit.Test
-    public void shouldRemainZeroWithAmountMoreBoundary() {
+    @ org.junit.jupiter.api. Test
+    void shouldRemainZeroWithAmountMoreBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1200;
         int expected = 0;
-        int actual = service.remain(amount);
+        int actual = service.remain(amount) ;
         assertEquals(expected, actual);
     }
 }
-
